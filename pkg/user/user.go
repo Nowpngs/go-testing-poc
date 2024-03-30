da5d30c29ca6da6d00cb8817b8071ebb000b39e6
Now <now.thongtangsai@gmail.com>
2024-03-30 22:54:54 +0700

    adding the role to user

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -4,8 +4,16 @@ import (
 	modal "go-testing-poc/pkg/abstract"
 )
 
+type Role int
+
+const (
+	AdminRole Role = iota
+	UserRole
+)
+
 type User struct {
 	modal.AbstractModal
 	Username string
 	Email    *string
+	Role     Role
 }
